89ad43011c6f6b54dd4f41c96500a0fd9cc44bc4
Marc kender Romel Jean Charles <makender103@gmail.com>
2023-03-04 20:25:15 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.scss'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <Home />
+  )
+}
+
+export default App
