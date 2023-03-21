308399eadec0f46c0aa16be2e95cea71dafdb5e3
Marc kender Romel Jean Charles <makender103@gmail.com>
2023-03-21 19:50:46 -0300

    feat: Custom Button Component

--- src/pages/Signup/FormSignUp.tsx
+++ src/pages/Signup/FormSignUp.tsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import BaseButton from '../../components/BaseButton';
 import BaseInput from '../../components/BaseInput'
+import CustomButton from '../../components/CustomButton';
 import { useAuthContext } from '../../context/authContext';
 
 export const FormSignUp = () => {
-  const {signUp} = useAuthContext();
+  const {signUp, loading = false} = useAuthContext();
 
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
@@ -22,7 +23,15 @@ export const FormSignUp = () => {
         <BaseInput label="Email" type='text' value={email} onChange={e =>setEmail(e)} />
         <BaseInput label="Password" type='password' value={password} onChange={e =>setPassword(e)} />
 
-        <BaseButton  label="Enskri"  width={150} background="#000052" color='#ffff'/>
+        <CustomButton
+          backgroundColor='#000052'
+          textColor='#ffff'
+          width={150}
+          isLoading={!!loading}
+          type="submit"
+        >
+            Enskri
+          </CustomButton>
     </form>
   )
 }
