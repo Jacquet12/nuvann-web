9e7706e9123646180e8acfbe7f45867b2ff64acf
Marc kender Romel Jean Charles <makender103@gmail.com>
2023-03-30 23:34:32 -0300

    fix: cart card initialisation

--- src/components/CartCard/index.tsx
+++ src/components/CartCard/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import { useCart } from '../../context/cartContext'
+import {MdDelete} from 'react-icons/md'
 import './styles.scss'
 
 interface CartCardProps {
+  items: any,
 
 }
-const CartCard: React.FC<CartCardProps> = ({}) => {
+const CartCard: React.FC<CartCardProps> = ({items}) => {
+  const {removeFromCart} = useCart();
+
   return (
+    <>
+    {
+      items.map((item:any, index:number) => (
     <div className="cart_card_container">
-        Welcome to the Cart
+        <img src={item?.product.images[0]} alt="" />
+        <div>img</div>
+        <div>
+        {/* onClick={removeFromCart(Number(item.id))} */}
+          <MdDelete color='red' size={22} />
+        </div>
     </div>
+
+      ))
+    }
+      </>
   )
 }
 export default CartCard;
